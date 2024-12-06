5cc61610259847de0372508e1428829c45d865c4
katya <katya@DESKTOP-F2MB8LV>
2024-12-06 17:10:13 -0600

    Agregar archivos de proyecto.

---
+++ mi app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace mi_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
